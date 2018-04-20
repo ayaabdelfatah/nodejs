763cc89b0071e912f88e9f37fa18c1ba54955b73
ayaabdelfatah <ayaabdelfatah@gmailcom>
2018-04-20 19:08:28 +0200

    aya

--- startup/mongodb.js
+++ startup/mongodb.js
@@ -1,7 +1,13 @@
+// const mongoose = require('mongoose');
+// let dbvar ='mongodb://localhost:27017/firstproj';
+// if(process.env.NODE_ENV === "production"){
+//     dbvar="con string from server";
+// }
 const mongoose = require('mongoose');
-let dbvar ='mongodb://localhost:27017/firstproj';
-if(process.env.NODE_ENV === "production"){
-    dbvar="con string from server";
-}
+let mongo_url = 'mongodb://localhost:27017/firstproj';
 
-mongoose.connect(dbvar);
+if(process.env.NODE_ENV === 'production'){
+    mongo_url = 'mongodb://admin:admin@ds251819.mlab.com:51819/firstproj'
+}
+mongoose.connect(mongo_url);
+//mongoose.connect(dbvar);
